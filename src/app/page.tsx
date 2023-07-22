472ba0fe240be2488af5e5c5301cb6e8a5486647
yspkm <yskim0830@outlook.com>
2023-07-22 22:34:40 +0900

    feat(layout): 디자인 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,33 +7,35 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
 	return (
-		<main className='flex flex-col w-full bg-white justify-center items-center py-10'>
+		<div className='w-full bg-white'>
 			<Navbar />
-			<CardNews />
+			<main className='flex flex-col justify-center items-center py-10'>
+				<CardNews />
 
-			<section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
-				<p className='text-center text-gray-500 text-sm mb-3'>
-					해시태그의 업데이트 주기는 24시간입니다.
-				</p>
-				<div className='sk-ww-linkedin-hashtag-posts' data-embed-id='170120'>
-					링크드인
-				</div>
-				<script src='https://widgets.sociablekit.com/linkedin-hashtag-posts/widget.js' async defer></script>
-			</section>
+				<section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
+					<p className='text-center text-gray-500 text-sm mb-3'>
+						해시태그의 업데이트 주기는 24시간입니다.
+					</p>
+					<div className='sk-ww-linkedin-hashtag-posts' data-embed-id='170120'>
+						링크드인
+					</div>
+					<script src='https://widgets.sociablekit.com/linkedin-hashtag-posts/widget.js' async defer></script>
+				</section>
 
-			<section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
-				<div className='sk-ww-instagram-hashtag-feed' data-embed-id='170285'>
-					인스타그램
-				</div>
-				<script src='https://widgets.sociablekit.com/instagram-hashtag-feed/widget.js' async defer></script>
-			</section>
+				<section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
+					<div className='sk-ww-instagram-hashtag-feed' data-embed-id='170285'>
+						인스타그램
+					</div>
+					<script src='https://widgets.sociablekit.com/instagram-hashtag-feed/widget.js' async defer></script>
+				</section>
 
-			<footer className='w-full text-center bg-gray-100 p-3 mt-10 border-t border-gray-200'>
-				<p className='text-gray-600 text-sm'>
-					Copyright © 2023 by SAP
-				</p>
-			</footer>
-		</main>
+				<footer className='w-full text-center bg-gray-100 p-3 mt-10 border-t border-gray-200'>
+					<p className='text-gray-600 text-sm'>
+						Copyright © 2023 by SAP
+					</p>
+				</footer>
+			</main>
+		</div>
 	);
 }
 
