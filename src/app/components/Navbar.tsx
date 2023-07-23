59a3b1a21a8846f5f7502b5047b69bfb5c4dbe50
yspkm <yskim0830@outlook.com>
2023-07-23 11:15:08 +0900

    style(layout.tsx): navbar만 bold로 하고 나머지는 얇게 유지함.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 	return (
 		<nav className='flex items-center justify-between p-2 md:px-4 text-4xl font-semibold py-3 bg-gradient-to-r from-cyan-500/75 to-blue-500/75'>
 			<a href='https://www.sap.com/korea/sustainability.html' 
-			   className='text-lg text-white font-extralight hover:text-gray-300 transition-colors duration-200' 
+			   className='text-lg text-white font-bold hover:text-gray-300 transition-colors duration-200' // 'font-extralight'를 'font-bold'로 변경했습니다.
 			   target='_blank'
 			   rel='noopener noreferrer'
 			>
