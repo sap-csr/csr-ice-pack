a0ef596c6b985421a0bcaf0be3ddf39feffa6b3f
yspkm <yskim0830@outlook.com>
2023-07-23 11:08:09 +0900

    style(font): 폰트 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import './globals.css';
-import { Noto_Sans_KR, Bagel_Fat_One } from 'next/font/google';
+import { Noto_Sans_KR } from 'next/font/google';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const notoSansKr = Noto_Sans_KR({
-	// preload: true, 기본값
-	subsets: ['latin'], // 또는 preload: false
-	weight: ['100', '400', '700', '900'], // 가변 폰트가 아닌 경우, 사용할 fontWeight 배열
+	subsets: ['latin'], 
+	preload: true,
+	weight: ['100', '400', '700', '900'], 
 });
-
-const bagelFatOne = Bagel_Fat_One({ subsets: ['latin'], weight: '400' })
 
 export const metadata = {
 	title: 'SAP 에코플랜터즈',
@@ -19,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
-			<body className={bagelFatOne.className}>{children}</body>
+			<body className={notoSansKr.className}>{children}</body>
 		</html>
 	);
 }
