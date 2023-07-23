cddbf4691ed22aac9e35412656e4c448ba486a9e
yspkm <yskim0830@outlook.com>
2023-07-23 13:07:12 +0900

    chore():

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 				<CardNews />
 
 				<section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/Pf1fMbGCDGI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Pf1fMbGCDGI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
 				</section>
 
 				<section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
