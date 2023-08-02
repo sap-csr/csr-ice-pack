6cd68d8700c750424a13e3b99d512d76ed684199
yspkm <yskim0830@outlook.com>
2023-08-03 01:15:34 +0900

    feat(instagram widget): 인스타그램 위젯 강조

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,16 @@ export default function Home() {
         <YoutubeCard href='https://www.youtube.com/embed/Pf1fMbGCDGI' />
 
         <section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
-          <p className='text-center text-gray-300 text-sm mb-3'>해시태그 업데이트 주기는 72h 입니다.</p>
+          <script src='https://snapwidget.com/js/snapwidget.js'></script>
+          <iframe src='https://snapwidget.com/embed/1039024' className='snapwidget-widget w-full'></iframe>
+        </section>
+
+        <section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
+          <p className='text-center text-gray-300 text-sm mb-3'>해시태그 업데이트 주기는 24h 입니다.</p>
           <div className='sk-ww-linkedin-hashtag-posts' data-embed-id='173384'></div>
           <script src='https://widgets.sociablekit.com/linkedin-hashtag-posts/widget.js' async defer></script>
         </section>
 
-        <section className='w-3/4 bg-gray-50 shadow-lg p-5 rounded-lg mt-5'>
-          <script src='https://snapwidget.com/js/snapwidget.js'></script>
-          <iframe src='https://snapwidget.com/embed/1039024' className='snapwidget-widget w-full'></iframe>
-        </section>
 
         <Footer />
       </main>
